cfe89b36b8971cdbe333a5680030c03e70fa7693
IamMikee <gamingwithmike2020@gmail.com>
2026-01-06 14:32:53 +0800

    Update Header.js

--- ppit20242025/src/app/Components/Header.js
+++ ppit20242025/src/app/Components/Header.js
@@ -22,50 +22,30 @@ const Header = () => {
 		<header
 			className={`fixed top-0 w-full h-[78px] flex justify-between items-center p-5 text-xl z-[49] ${
 				isScrolled
-					? "bg-[#FFFFFF]"
+					? "bg-[#8C0000]"
 					: "bg-gradient-to-b from-black to-transparent"
 			}`}
 		>
 			{/* Logo */}
 			<Link href="/" className="flex items-center">
-				{isScrolled ? (
-					<>
-					<img
-						src="/PPITLOGO.webp"
-						alt="Logo"
-						className="w-[55px] h-[80px] object-contain"
-					/>
+				<img
+					src="/ppitsz_whitelogo.webp"
+					alt="Logo"
+					className="w-[75px] h-[106px] object-contain"
+				/>
 
-					<img
-						src="/ppitsz2526_redlogo.webp"
-						alt="Logo"
-						className="w-[57px] h-[64px] object-contain"
-					/>
+				<img
+					src="/ppitsz2526_whitelogo.webp"
+					alt="Logo"
+					className="w-[38px] h-[54px] object-contain"
+				/>
 
-					<span className="text-[#8C0000] font-cinzel text-[26px] font-bold ml-1">PPIT SHENZHEN</span>
-					</>
-				) : (
-					<>
-					<img
-						src="/ppitsz_whitelogo.webp"
-						alt="Logo"
-						className="-ml-3 w-[75px] h-[106px] object-contain"
-					/>
-
-					<img
-						src="/ppitsz2526_whitelogo.webp"
-						alt="Logo"
-						className="w-[38px] h-[54px] object-contain"
-					/>
-
-					<span className="text-white font-cinzel text-[26px] font-bold ml-4">PPIT SHENZHEN</span>
-					</>
-				)}
+				<span className="text-white font-cinzel text-[26px] font-bold ml-4">PPIT SHENZHEN</span>
 			</Link>
 
 			{/* Overlay for mobile menu */}
 			<div
-				className={`fixed top-0 right-0 w-[45%] h-full bg-white z-10 ${
+				className={`fixed top-0 right-0 w-[45%] h-full bg-black/70 z-10 transition-opacity ${
 					menuOpen ? "block" : "hidden"
 				}`}
 				onClick={toggleMenu}
@@ -79,98 +59,52 @@ const Header = () => {
 				aria-label="Toggle navigation"
 				onClick={toggleMenu}
 			>
-				{isScrolled ? (
-          		<>
-					<span
-						className={`block w-full h-[3px] bg-[#8C0000] rounded transition-transform origin-center ${
-							menuOpen ? "transform translate-y-[11px] rotate-45" : ""
-						} `}
-					></span>
-					<span
-						className={`block w-full h-[3px] bg-[#8C0000] rounded transition-opacity ${
-							menuOpen ? "opacity-0" : ""
-						} `}
-					></span>
-					<span
-						className={`block w-full h-[3px] rounded bg-[#8C0000] transition-transform origin-center ${
-							menuOpen ? "transform translate-y-[-11px] -rotate-45" : ""
-						} `}
-					></span>
-				</>
-				) : (
-				<>
-					<span
-						className={`block w-full h-[3px] rounded transition-transform origin-center ${
-							menuOpen ? "transform translate-y-[11px] rotate-45 bg-[#8C0000]" : "bg-white"
-						} `}
-					></span>
-					<span
-						className={`block w-full h-[3px] bg-white rounded transition-opacity ${
-							menuOpen ? "opacity-0" : ""
-						} `}
-					></span>
-					<span
-						className={`block w-full h-[3px] rounded transition-transform origin-center ${
-							menuOpen ? "transform translate-y-[-11px] -rotate-45 bg-[#8C0000]" : "bg-white"
-						} `}
-					></span>
-				</>
-				)}
+				<span
+					className={`block w-full h-[3px] bg-white rounded transition-transform origin-center ${
+						menuOpen ? "transform translate-y-[11px] rotate-45" : ""
+					} `}
+				></span>
+				<span
+					className={`block w-full h-[3px] bg-white rounded transition-opacity ${
+						menuOpen ? "opacity-0" : ""
+					} `}
+				></span>
+				<span
+					className={`block w-full h-[3px] bg-white rounded transition-transform origin-center ${
+						menuOpen ? "transform translate-y-[-11px] -rotate-45" : ""
+					} `}
+				></span>
 			</button>
 
 			{/* Navigation menu */}
 			<nav
 				className={`md:flex gap-9 ${
 					menuOpen
-						? "flex flex-col absolute top-[70px] right-0 w-[43%] p-5 text-[#8C0000] shadow-lg z-[999]"
+						? "flex flex-col absolute top-[70px] right-0 w-[43%] p-5 text-white shadow-lg z-[999]"
 						: "hidden"
 				}`}
 			>
 				<Link
 					href="/"
-					className={`md:text-2xl text-xl hover:text-[#8C0000] font-montserrat font-semibold ${
-						isScrolled && !menuOpen
-							? "text-[#8C0000]"
-							: menuOpen
-							? "text-[#8C0000]"
-							: "text-white"
-					}`}
+					className="md:text-2xl text-xl hover:text-[#b30000] font-montserrat font-semibold text-white"
 				>
 					Home
 				</Link>
 				<Link
 					href="/committee"
-					className={`md:text-2xl text-xl hover:text-[#8C0000] font-montserrat font-semibold ${
-						isScrolled && !menuOpen
-							? "text-[#8C0000]"
-							: menuOpen
-							? "text-[#8C0000]"
-							: "text-white"
-					}`}
+					className="md:text-2xl text-xl hover:text-[#b30000] font-montserrat font-semibold text-white"
 				>
 					Committee
 				</Link>
 				<Link
 					href="/events"
-					className={`md:text-2xl text-xl hover:text-[#8C0000] font-montserrat font-semibold ${
-						isScrolled && !menuOpen
-							? "text-[#8C0000]"
-							: menuOpen
-							? "text-[#8C0000]"
-							: "text-white"
-					}`}
+					className="md:text-2xl text-xl hover:text-[#b30000] font-montserrat font-semibold text-white"
 				>
 					Events
 				</Link>
 				<Link
 					href="/faq"
-					className={`md:text-2xl text-xl hover:text-[#8C0000] font-montserrat font-semibold ${
-						isScrolled && !menuOpen
-							? "text-[#8C0000]"
-							: menuOpen
-							? "text-[#8C0000]"
-							: "text-white"
-					}`}
+					className="md:text-2xl text-xl hover:text-[#b30000] font-montserrat font-semibold text-white"
 				>
 					FAQ
 				</Link>
