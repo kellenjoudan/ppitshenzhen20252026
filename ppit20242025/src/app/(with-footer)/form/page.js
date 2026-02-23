b4001547083715f82b67cbb9ee173db84fcccad2
IamMikee <gamingwithmike2020@gmail.com>
2026-02-23 20:06:49 +0700

    Update page.js

--- ppit20242025/src/app/(with-footer)/form/page.js
+++ ppit20242025/src/app/(with-footer)/form/page.js
@@ -86,7 +86,7 @@ export default function loadAllFormsPage(){
 
     if (loading) return (
         <div style={{ minHeight: "100vh", backgroundColor: "#7E0C0E", margin: 0, padding: 0 }}>
-            <div className="font-montserrat text-white" style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "1.5rem" }}>Fetching forms, please wait...</div>
+            <div className="font-montserrat text-white" style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "1.5rem", textAlign: "center" }}>Fetching forms, please wait...</div>
         </div>
 
     );
@@ -94,7 +94,7 @@ export default function loadAllFormsPage(){
     if (activeForms.length == 0 && !admin) {
         return (
             <div style={{ minHeight: "100vh", backgroundColor: "#7E0C0E", fontFamily: "Arial, sans-serif", margin: 0, padding: 0 }}>
-                <div className="font-montserrat text-white" style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "1.5rem" }}>No events to register at the moment.</div>
+                <div className="font-montserrat text-white" style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "1.5rem", textAlign: "center" }}>No events to register at the moment.</div>
             </div>
         )
     }
