75cb6db903e5115bac93853153a111cf9e61cdb8
IamMikee <gamingwithmike2020@gmail.com>
2026-04-16 03:32:05 +0800

    fix mobile popup issues

--- ppit20242025/src/app/(with-footer)/form/[formId]/responses/page.js
+++ ppit20242025/src/app/(with-footer)/form/[formId]/responses/page.js
@@ -171,7 +171,15 @@ const openInSheets = async () => {
         const data = await res.json();
 
         if (data.url) {
-            window.open(data.url, "_blank");
+            // Fixes pop-up issues with mobile browsers
+            const link = document.createElement("a");
+            link.href = data.url;
+            link.target = "_blank";
+            link.rel = "noopener noreferrer";
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         }
     } catch(e) {
         console.error("network error: "+ e);
